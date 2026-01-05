97f488cb6ebfe1afbf4b6cd2753137d1b6d2d112
arthurarthure <arthurmukiibi08@gmail.com>
2026-01-05 19:50:56 +0100

    module 5 push 5 pre D-Day

--- Kuro.js
+++ Kuro.js
@@ -33,13 +33,14 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
+    const track = document.querySelector(".slideshow-track");
+    const slides = document.querySelectorAll(".slide");
     let index = 0;
-        const track = document.querySelector(".slideshow-track");
-        const slides = document.querySelectorAll(".slide");
 
-        setInterval(() => {
+    setInterval(() => {
         index = (index + 1) % slides.length;
         track.style.transform = `translateX(-${index * 100}%)`;
         }, 3000);
+
 
 });
